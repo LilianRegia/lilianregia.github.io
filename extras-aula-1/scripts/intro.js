8a8dcb8a89657baf15b167c12a75e046ae3f91e0
Lílian Régia Alves <65667670+LilianRegia@users.noreply.github.com>
2020-06-04 18:56:35 -0300

    Add files via upload

---
+++ extras-aula-1/scripts/intro.js
@@ -0,0 +1,69 @@
+// comments
+// document.write("Hello")
+
+// variaveis, tipos de dados
+// let myvar = "He"
+// const myconst = "He"
+
+// document.write(myconst + myvar)
+
+// string
+// "Isso Ã© uma string"
+// 'Isso tambÃ©m Ã© um string'
+// `Isso Ã© uma string tambÃ©m`
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// objeto possuem propriedades e funcionalidade
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("executar uma grande logica de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+// Array - Vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+
+// FunÃ§Ãµes - Funcionalides - Atalhos - Reuso de cÃ³digo
+
+// // registrar funÃ§Ã£o
+// function sayMyName(name) {
+  
+//   document.write(name)
+// }
+
+// // executar
+// sayMyName("Douglas")
+// sayMyName("Valeska")
+// sayMyName("Kyam")
+
+// condicionais
+
+// const notaFinal = 7
+
+// if( notaFinal < 5 ) {
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+// loop repetiÃ§Ãµes
+// for (i = 0; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
